cb9b13d9219198668cf5acbe53e18f21ed5a0e61
beeka <ahmed.abdelgawad.farag@gmail.com>
2020-08-08 23:32:15 +0200

    clean some code

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -5,14 +5,14 @@ class UserSessionController < ApplicationController
   end
 
   def create
-    if (login(params[:user][:email], params[:user][:password]))
+    if login params[:user][:email], params[:user][:password]
       redirect_to posts_path
     end
   end
 
   def login(email, password)
     user = User.find_by_email(email)
-    if (user != nil && user.password == password)
+    if user != nil && user.password == password
       return true
     else
       return false
