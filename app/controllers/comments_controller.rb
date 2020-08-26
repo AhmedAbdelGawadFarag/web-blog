8891e38821900df2dd44fad33b81a1643e2d0dfb
beeka <ahmed.abdelgawad.farag@gmail.com>
2020-08-26 12:43:18 +0200

    now you can see who post a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     postid = flash[:post_id]
     comment = Comment.new(params.require(:comment).permit("body"))
     comment.post_id = postid
+    comment.user_name = current_user.name
     comment.save()
     redirect_to post_url(postid)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_212835) do
+ActiveRecord::Schema.define(version: 2020_08_26_102151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_212835) do
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
     t.bigint "post_id", null: false
+    t.string "user_name"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
